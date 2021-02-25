d11de8d98062e06e27ffa840edd626fbb4749300
ghendersonnc <ghendersonnc@gmail.com>
2021-02-25 16:15:29 -0500

    Added object detection to detect ifunny watermark

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 # iFunny Watermark Remover
 
-The name is a bit deceiving as it simply just removes the bottom 20 pixels from an image
+This now detects the iFunny watermark!
 
-However, this was done with the intent to use it remove the iFunny watermark.
-
-This is NOT an AI or ML program
-
-This does NOT detect the presence of any sort of watermark
-
-This simply removes the bottom 20 pixels from an image
+New images will be generated appended with _nomark
---
+++ main.py
@@ -0,0 +1,48 @@
+import cv2 as cv
+import numpy as np
+import os
+
+"""
+Detects and removes iFunny watermark from images
+"""
+
+def detect_mark(img):
+    watermark = cv.imread('assets/watermark.png', cv.IMREAD_UNCHANGED)
+    result = cv.matchTemplate(img, watermark, cv.TM_CCOEFF_NORMED)
+
+    threshold = .90
+
+    # Add regions that have confidence above or at 90%
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+
+    if locations:
+        return True
+    else:
+        return False
+
+def main():
+    image_list = []
+    for root, dirs, files in os.walk("./images"):
+        for filename in files:
+            temp = filename.split('.')
+            image_list.append(tuple(temp))
+    
+    for image in image_list:
+        if image[1] not in ["jpg", "png"]:
+            continue
+
+        img = cv.imread("images/{}".format(image[0] + "." + image[1]), cv.IMREAD_COLOR)
+
+        if (detect_mark(img)):
+            cropped_image = img[0:img.shape[0]-20, 0:img.shape[1]]
+            cropped_image_filename = "{}_nomark{}".format(image[0], "." + image[1])
+            cv.imwrite("images/{}".format(cropped_image_filename), cropped_image)
+        else:
+            continue
+
+
+
+
+if __name__ == "__main__":
+    main()
